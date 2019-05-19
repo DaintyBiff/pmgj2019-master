bd04bbe7802de5f109607fb05cb872505062a65d
DaintyBiff <rjessel@alumni.berklee.edu>
2019-05-19 18:42:05 -0400

    Don't ignore the build folder

---
+++ build/precache-manifest.577400308dfe8bc99f4fee0c19af8b42.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8841cd92d2ef18314f5ee68bb8030f1",
+    "url": "./assets/audio/sfx/Purple Monkey Scream 2.mp3"
+  },
+  {
+    "revision": "235703beaebbc8b042bb",
+    "url": "./js/bundle.js"
+  },
+  {
+    "revision": "97f2be1edec532c5839b",
+    "url": "./js/vendor.bundle.js"
+  },
+  {
+    "revision": "e08de18920eb928127f70af8decfd8b5",
+    "url": "./assets/audio/sfx/Purple Monkey Scream 3.mp3"
+  },
+  {
+    "revision": "e6037d6d0737bada8682bb5a52866bad",
+    "url": "./assets/images/menu/graphic.png"
+  },
+  {
+    "revision": "86fef4cc8a6bf1cca6cb11f63454c6cc",
+    "url": "./assets/images/menu/tilesprite.png"
+  },
+  {
+    "revision": "e2476e42d4a0622c03571330ba0a77f9",
+    "url": "./assets/images/ryu/ryu.gif"
+  },
+  {
+    "revision": "263c98f0d4e13da4c4b8a0cfc2d84122",
+    "url": "./assets/images/naturey.png"
+  },
+  {
+    "revision": "f8ba06c323a8fc17c0044df6df948a1f",
+    "url": "./assets/images/phaser-es6-webpack.jpg"
+  },
+  {
+    "revision": "2c0cd501a0b10c617f95273e083f7dec",
+    "url": "./assets/images/ryu/forwardjump-74x102x7.png"
+  },
+  {
+    "revision": "27b5e0541f20ca6f173f80dadeba08fa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8dfe30a506fb0721a75523632afdd462",
+    "url": "./assets/players.json"
+  },
+  {
+    "revision": "966d492aff4fc7baa33302700dd7e986",
+    "url": "./assets/images/sword.png"
+  },
+  {
+    "revision": "2afd1f4a931155e427188ced29430feb",
+    "url": "./assets/images/ryu/walking-56x96x5.png"
+  },
+  {
+    "revision": "1a51791217759fde26d3a062fd44fe63",
+    "url": "./assets/images/ryu/lpunch-62x96x3.png"
+  },
+  {
+    "revision": "fa886f1c2fdec9bef2e4cafc08649f4d",
+    "url": "./assets/images/ryu/victory-52x110x5.png"
+  },
+  {
+    "revision": "7b9c40f68a164a90f211a3d8d8f9c350",
+    "url": "./assets/images/ryu/idle-53x96x4.png"
+  },
+  {
+    "revision": "1a51791217759fde26d3a062fd44fe63",
+    "url": "./assets/images/ryu/mpunch-62x96x3.png"
+  },
+  {
+    "revision": "71046567fbfacc785ea49cc6041a6763",
+    "url": "./assets/images/ryu/jumphkick-56x96x3.png"
+  },
+  {
+    "revision": "dad66e091eecc38e4ce56af3a9ad3e80",
+    "url": "./assets/images/mushroom2.png"
+  },
+  {
+    "revision": "3a3ad34877eba28e3afe4818bc3bd5eb",
+    "url": "./assets/images/menu/title.png"
+  },
+  {
+    "revision": "ab5d4ee6e76a11cee700b328d25e28a0",
+    "url": "./assets/images/naturey-collision.png"
+  },
+  {
+    "revision": "848b2954d203ac7953ab8d5cf71f0fcc",
+    "url": "./assets/images/new-game-button.png"
+  },
+  {
+    "revision": "3b89b9c2964d19f592309460cdb9e6be",
+    "url": "./assets/images/red-block.png"
+  },
+  {
+    "revision": "cd4eadd1a34bd1a05ecf35e9752b4ebe",
+    "url": "./assets/images/ryu/defeat-75x96x4.png"
+  },
+  {
+    "revision": "8a34f9298a13fc9b9589886cc092c2f0",
+    "url": "./assets/images/ryu/hpunch-75x96x5.png"
+  },
+  {
+    "revision": "c7b1606e39f1dda38b493a344cb080d1",
+    "url": "./assets/audio/sfx/Purple Monkey Med Punch 2 Muffled.mp3"
+  },
+  {
+    "revision": "3daf57f06d883f41fdb3556e9721e3c5",
+    "url": "./assets/images/menu/button.png"
+  },
+  {
+    "revision": "33cfb21e6af35e3bff4ada7eb4d124d2",
+    "url": "./assets/audio/sfx/Purple Monkey Med Punch 3.mp3"
+  },
+  {
+    "revision": "dfb749fb41edc5c53626b50ec9d48776",
+    "url": "./assets/images/loader-bg.png"
+  },
+  {
+    "revision": "21e9437b78d613f461d84e1049f432c9",
+    "url": "./assets/audio/sfx/Purple Monkey Punch High.mp3"
+  },
+  {
+    "revision": "4397b60ccecb550f5ad25c83833cebdd",
+    "url": "./assets/images/chunli/mpunch-90-96-3.png"
+  },
+  {
+    "revision": "426fd9061903e7f37deff79169ad760c",
+    "url": "./assets/images/chunli/walking-58x96x8.png"
+  },
+  {
+    "revision": "e251becd9d92981d81c66682c3dfbf69",
+    "url": "./assets/images/chunli/victory-52x140x5.png"
+  },
+  {
+    "revision": "6eb0c377e0ea2aac41ac8d8b229a8020",
+    "url": "./assets/images/icon-192px.png"
+  },
+  {
+    "revision": "c97d238339edea4c33afd5a7f6a0d8ce",
+    "url": "./assets/images/icon-512px.png"
+  },
+  {
+    "revision": "78ea0555ef7648bf378481a5d778d344",
+    "url": "./assets/images/loader-bar.png"
+  },
+  {
+    "revision": "1f9bf6571f6bb2537fc5bcdfa5c6b7ac",
+    "url": "./assets/audio/sfx/Purple Monkey Scream 5.mp3"
+  },
+  {
+    "revision": "cfabdd07f93a99a0fe51e343ae02884d",
+    "url": "./assets/audio/sfx/Purple Monkey Scream 4.mp3"
+  },
+  {
+    "revision": "bd001278b79138e9b87fee2bc6c01690",
+    "url": "./assets/images/chunli/forwardjump-68x102x8.png"
+  },
+  {
+    "revision": "88faa6327dedbaaf429a84486dc5ff4b",
+    "url": "./assets/images/chunli/defeat-80-96-4i.png"
+  },
+  {
+    "revision": "c4ede5f1e8cbc10a385f6e5708c4a581",
+    "url": "./assets/images/chunli/chunli.gif"
+  },
+  {
+    "revision": "a0ab1065b699cf51f7b3e688568e5fe9",
+    "url": "./assets/audio/sfx/Square FX Victory.mp3"
+  },
+  {
+    "revision": "4b0c793824a795c1053ce40424e53bec",
+    "url": "./assets/audio/sfx/Square FX Defeat.mp3"
+  },
+  {
+    "revision": "2d13c7ae4cd95dc41e01aa6104e46c77",
+    "url": "./assets/images/chunli/hpunch-90-96-3.png"
+  },
+  {
+    "revision": "0be54c6c79efdb772c717b58fceba30d",
+    "url": "./assets/images/chunli/idle-53x96x4.png"
+  },
+  {
+    "revision": "51e8a720913d3805d5575d2abd299725",
+    "url": "./assets/audio/sfx/Purple Monkey Square FX 2.mp3"
+  },
+  {
+    "revision": "6f79cdb851d36267863058d7c0862f89",
+    "url": "./assets/audio/sfx/Purple Monkey Square FX 1.mp3"
+  },
+  {
+    "revision": "87b640945aff156487812503267a9ca8",
+    "url": "./assets/images/chunli/jumphkick-66x100x6.png"
+  },
+  {
+    "revision": "ec83972242b610bf60585bfd79c5749a",
+    "url": "./assets/images/chunli/lpunch-65x96x3.png"
+  },
+  {
+    "revision": "77304c2015a4464c55815e65ddc74e42",
+    "url": "./assets/audio/sfx/Purple Monkey Instrumental Stinger Victory.mp3"
+  },
+  {
+    "revision": "ddde46e8ad547160bcfa0c3c8d6cc39c",
+    "url": "./assets/audio/sfx/Purple Monkey Scream 1.mp3"
+  },
+  {
+    "revision": "f7e627022b47ae2f71fdf1d5b0440cd6",
+    "url": "./assets/audio/sfx/Purple Monkey Crowd.mp3"
+  },
+  {
+    "revision": "42d2eeeccc82b7b116066a8f4ec8ae4f",
+    "url": "./assets/audio/sfx/Purple Monkey Punch Med.mp3"
+  },
+  {
+    "revision": "a7772e4b2fbae674749a990181a7f18c",
+    "url": "./assets/audio/sfx/Purple Monkey Grunt 3 More Muffled.mp3"
+  },
+  {
+    "revision": "551fd7e5491e925081046ad4f4249e5e",
+    "url": "./assets/audio/sfx/Purple Monkey Low Punch 2.mp3"
+  },
+  {
+    "revision": "03493352de0cb260b4daa6ecd7e20e77",
+    "url": "./assets/audio/sfx/Purple Monkey Low Punch 3 Muffled.mp3"
+  },
+  {
+    "revision": "521f651815e44ef36c92b4276e3b1e8e",
+    "url": "./assets/audio/sfx/Purple Monkey Punch Low.mp3"
+  },
+  {
+    "revision": "9d08cf9b330b58bf7fd8a3e0bb1689f7",
+    "url": "./assets/audio/.DS_Store"
+  },
+  {
+    "revision": "dc69015211541254e1c7f273536ed536",
+    "url": "./assets/audio/sfx/Purple Monkey Grunt 4.mp3"
+  },
+  {
+    "revision": "6c927d940a4b40141f5acba6d6ab6903",
+    "url": "./assets/audio/sfx/Purple Monkey High Punch 2 Muffled.mp3"
+  },
+  {
+    "revision": "3de3b8032dab44e35da6ac09d97edce6",
+    "url": "./assets/audio/sfx/Purple Monkey Grunt.mp3"
+  },
+  {
+    "revision": "283a45b0d9d2b1142f3cceeb42130bf2",
+    "url": "./assets/audio/sfx/Purple Monkey High Punch 3.mp3"
+  },
+  {
+    "revision": "f1817fa2dc91daf2784db500f1e958d3",
+    "url": "./assets/audio/sfx/Purple Monkey Grunt 2.mp3"
+  },
+  {
+    "revision": "256edf217c37b7df3d4f46abd1d73a17",
+    "url": "./assets/audio/loops/Purple Monkey Loop 4 Revised.mp3"
+  },
+  {
+    "revision": "36a0190b25ab51467ed50493360b7f63",
+    "url": "./assets/audio/loops/Purple Monkey Loop 3.mp3"
+  },
+  {
+    "revision": "6b8e2d589875c4c0cb103f52d39c2a99",
+    "url": "./assets/audio/loops/Purple Monkey Loop 2 Slower Maybe Menu.mp3"
+  },
+  {
+    "revision": "4bc73f501a337912c1f3bd44b97db08f",
+    "url": "./assets/audio/loops/Purple Monkey Loop 1.mp3"
+  },
+  {
+    "revision": "2e33ae68b8422d0320ba158bba62fee3",
+    "url": "./assets/.DS_Store"
+  },
+  {
+    "revision": "3585bc041684e8845b639088608b3bb2",
+    "url": "././manifest.json"
+  }
+];
